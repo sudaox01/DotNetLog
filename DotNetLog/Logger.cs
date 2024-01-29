f5a3891574e8e63fdd398199ac6784c7d6d334d0
sudaox01 <121214747+sudaox01@users.noreply.github.com>
2024-01-29 18:56:44 -0500

    Add beta files

---
+++ DotNetLog/Logger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DotNetLog
+{
+    public class Logger
+    {
+        // Config, Configure your options here. This will support embedded app XML if used as a DLL soon, This is planned but this project is currently still in the works
+        // and needs a lot of bug fixing, features, etc. It is nowhere near complete yet.
+
+        private static bool LogLib_FileLog = true; // False - Don't log to file, true - Log to file
+        private static string LogLib_FileName = "log.log"; // File to log to
+        private static void doLog(int logType, string logMsg, string className)
+        {
+            // 0 - Info, 1 - Debug, 2 - Error, 3 - Warning
+            string logLabel = "";
+            // Error Log
+            // Set Log Info
+            switch (logType)
+            {
+                case 0:
+                    // Info, colour default
+                    logLabel = "INFO";
+                    break;
+                case 1:
+                    // Debug, same as info
+                    logLabel = "DEBUG";
+                    break;
+                case 2:
+                    // Error, This is where we add some red to the mix
+                    logLabel = "ERROR";
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case 3:
+                    // Warning, we add a tiny bit of yellow! :3
+                    logLabel = "WARN";
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+            }
+            DateTime dt = DateTime.Now;
+            string timeVal = dt.ToLongDateString() + " " + dt.ToLongTimeString();
+            string tFormat = "[" + logLabel + "] " + "(" + timeVal + ") " + className + ": " + logMsg;
+            Console.WriteLine(tFormat);
+            Console.ResetColor();
+            if (LogLib_FileLog)
+            {
+                // Log to File
+                // Get contents of old file if exists
+                string cSpec = "";
+                if (File.Exists(LogLib_FileName))
+                {
+                    cSpec = File.ReadAllText(LogLib_FileName);
+                }
+                cSpec = tFormat + "\r\n" + cSpec;
+                File.WriteAllText(LogLib_FileName, cSpec);
+            }
+        }
+        // These are just stub functions to redirect to doLog easily
+        public static void Info(string logMsg, string className = "")
+        {
+            doLog(0, logMsg, className);
+        }
+        public static void Debug(string logMsg, string className = "")
+        {
+            doLog(1, logMsg, className);
+        }
+        public static void Error(string logMsg, string className = "")
+        {
+            doLog(2, logMsg, className);
+        }
+        public static void Warn(string logMsg, string className = "")
+        {
+            doLog(3, logMsg, className);
+        }
+    }
+}
